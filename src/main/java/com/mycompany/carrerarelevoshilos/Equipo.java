5937547f1575a37d4f9fc675e1d9de06e9a402c9
edwardconw <coyoteluxeredward@outlook.es>
2019-04-07 02:13:57 -0500

    Imprecion de Puesto, creacion de variables Get set y constructor

--- src/main/java/com/mycompany/carrerarelevoshilos/Equipo.java
+++ src/main/java/com/mycompany/carrerarelevoshilos/Equipo.java
@@ -11,4 +11,81 @@ package com.mycompany.carrerarelevoshilos;
  */
 public class Equipo {
     
+    
+    private String nombreEquipo;
+    private int posicion;
+    private int posicion1;
+    private int posicion2;
+    private int posicion3;
+    
+    public Equipo(String nombreEqui) {
+        this.nombreEquipo = nombreEqui;
+        this.posicion1=0;
+        this.posicion2=30;
+        this.posicion3=60;
+    }
+    
+      
+     public synchronized String imprimirPuesto(){
+        String puesto="";
+        
+        puesto = "Equipo : " + nombreEquipo;
+        
+        for (int i = 0; i <= 100; i++) {
+            if (i == posicion1) {
+                puesto += "X";
+            } else if (i == posicion2) {
+                puesto += "O";
+            } else if (i == posicion3) {
+                puesto += "+";
+            } else {
+                puesto += "_";
+            }
+        } 
+        return puesto;
+    }
+     
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+     
+     public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public int getPosicion1() {
+        return posicion1;
+    }
+
+    public void setPosicion1(int posicion1) {
+        this.posicion1 = posicion1;
+    }
+
+    public int getPosicion2() {
+        return posicion2;
+    }
+
+    public void setPosicion2(int posicion2) {
+        this.posicion2 = posicion2;
+    }
+
+    public int getPosicion3() {
+        return posicion3;
+    }
+
+    public void setPosicion3(int posicion3) {
+        this.posicion3 = posicion3;
+    }
+                                                
+    
 }
+
+
