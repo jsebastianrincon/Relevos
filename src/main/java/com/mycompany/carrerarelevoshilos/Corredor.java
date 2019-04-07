70a63dc1d5229292eb5baeea1c2234e0e50cd7fa
edwardconw <coyoteluxeredward@outlook.es>
2019-04-07 11:26:19 -0500

    Corredor 3

--- src/main/java/com/mycompany/carrerarelevoshilos/Corredor.java
+++ src/main/java/com/mycompany/carrerarelevoshilos/Corredor.java
@@ -52,6 +52,24 @@ public class Corredor extends Thread{
         }
     }
     
+    public void avanzaCorredor3(){
+        while (true) {
+            int posicion = correr(3);
+            if (posicion >= 100) {
+                equipo.setPosicion3(100);
+                if(equipo.getNombreEquipo().equals("Equipo1")){
+                    equipo.setNombreEquipo("Equipo 1");
+                }else if(equipo.getNombreEquipo().equals("Equipo2")){
+                    equipo.setNombreEquipo("Equipo 2");
+                }else if(equipo.getNombreEquipo().equals("Equipo3")){
+                    equipo.setNombreEquipo("Equipo 3");
+                }
+                System.out.println("El " + equipo.getNombreEquipo()+" Gana la carrera !!!");
+                break;
+            }
+        }
+    }
+    
      public void esperar(){       
         synchronized (equipo) {           
             try {
