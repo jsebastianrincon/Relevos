ee8a7da086592430fb865091fd9f1d990f03cc1d
jsebastianrincon <47905622+jsebastianrincon@users.noreply.github.com>
2019-04-07 02:37:55 -0500

    Metodo imprimir equipo

--- src/main/java/com/mycompany/carrerarelevoshilos/Corredor.java
+++ src/main/java/com/mycompany/carrerarelevoshilos/Corredor.java
@@ -9,6 +9,41 @@ package com.mycompany.carrerarelevoshilos;
  *
  * @author SuperUs
  */
-public class Corredor {
+public class Corredor extends Thread{
+    Equipo equipo;
+    private String nombreEquipo;
+    private int posiciondeInicio;
+    private int posiciondeLlegada;
+    private String equipo1;
+    private String equipo2;
+    private String equipo3;
+    
+    public Corredor(String nombreEquipo, int posiciondeInicio, int posiciondeLlegada, Equipo equipo ){
+        this.nombreEquipo = nombreEquipo;
+        this.posiciondeInicio = posiciondeInicio;
+        this.posiciondeLlegada = posiciondeLlegada;
+        this.equipo = equipo;
+    }
+    
+    public void mostrarEquipo(){
+        
+        if(equipo.imprimirPuesto().contains("Equipo1")){
+            equipo1 = equipo.imprimirPuesto();
+                if(equipo1 != null){
+                    system.out.println(equipo1);
+                }
+        }else if(equipo.imprimirPuesto().contains("Equipo2")){
+            equipo2=equipo.imprimirPuesto();
+                if(equipo2 != null){
+                    system.out.println(equipo2);
+                }
+        }else if(equipo.imprimirPuesto().contains("Equipo3")){
+            equipo3=equipo.imprimirPuesto();
+                 if(equipo3 != null){
+                     system.out.println(equipo3);
+                   }
+        }
+        
+    }
     
 }
